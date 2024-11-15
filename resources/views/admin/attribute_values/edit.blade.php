07d110440feb02841755e91445dd8f0a629f9d44
dungnguyenngoc4 <dungngyanh4@gmail.com>
2024-11-15 21:58:45 +0700

    update

--- resources/views/admin/attribute_values/edit.blade.php
+++ resources/views/admin/attribute_values/edit.blade.php
@@ -18,7 +18,7 @@
 
                 <!-- Input cho Giá trị thuộc tính -->
                 <div class="mt-3 mb-3 d-flex align-items-center">
-                    <label for="name" class="form-label me-2">Giá trị thuộc tính</label>
+                    <label for="name" class="form-label me-2">Tên giá trị thuộc tính</label>
                     <input type="text" name="name" class="form-control @error('name') is-invalid @enderror"
                         value="{{ old('name', $attribute_value->name) }}">
                     <!-- Sử dụng old() với giá trị từ $attribute_value -->
@@ -30,7 +30,7 @@
                 <!-- Kiểm tra và hiển thị input chọn màu nếu là thuộc tính "color" -->
                 @if ($type_name === 'color')
                     <div class="mt-3 mb-2 d-flex align-items-center">
-                        <label for="colorValue" class="form-label me-2">Chọn màu</label>
+                        <label for="colorValue" class="form-label me-2">Giá trị thuộc tính</label>
                         <div class="input-group">
                             <input type="color"
                                 class="form-control form-control-color @error('value') is-invalid @enderror" id="colorValue"
